72b0b18168d347a166b12358c9d0c7f4f8e704cf
ChoRyan Quan <choryanquan@163.com>
2021-06-24 16:14:56 +0800

    fix: 1.以圆心为基础向内/外draw

--- app/src/main/assets/vertex/vertex_draw_circle_shader.glsl
+++ app/src/main/assets/vertex/vertex_draw_circle_shader.glsl
@@ -6,17 +6,23 @@ varying vec4 v_texturePos;
 varying vec4 f_textureColor;
 
 void main() {
-//    v_texturePos = u_Matrix * av_Position;
-//    gl_Position = v_texturePos;
-//    f_textureColor = af_Color;
+    //    v_texturePos = u_Matrix * av_Position;
+    //    gl_Position = v_texturePos;
+    //    f_textureColor = af_Color;
 
     //***************
 
     v_texturePos = u_Matrix * av_Position;
-    if (v_texturePos == vec4(0.0, 0.0, 0.0, 1.0)){
+
+    vec4 center = vec4(0.0, 0.0, 0.0, 0.0);
+
+    float distance = sqrt(pow(v_texturePos.x, 2.0) + pow(v_texturePos.y, 2.0));
+
+    if (distance <= 0.5){
         f_textureColor = vec4(1.0, 1.0, 1.0, 1.0);
     } else {
         f_textureColor = af_Color;
     }
     gl_Position = v_texturePos;
+
 }
