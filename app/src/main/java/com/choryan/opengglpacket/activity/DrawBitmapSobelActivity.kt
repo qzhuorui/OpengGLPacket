771c9c4ab1997961c7d2d4458882228ed0c6b6e0
ChoRyan Quan <choryanquan@163.com>
2021-07-06 12:06:13 +0800

    add: 1. Sobel算子边缘检测

--- app/src/main/java/com/choryan/opengglpacket/activity/DrawBitmapSobelActivity.kt
+++ app/src/main/java/com/choryan/opengglpacket/activity/DrawBitmapSobelActivity.kt
@@ -31,6 +31,7 @@ class DrawBitmapSobelActivity : BaseActivity(R.layout.activity_draw_bitmap_sobel
         btn_render.setOnClickListener {
             val curFilter = GPUImageSobelEdgeDetectionFilter()
             v_gpuimage_view.setFilter(curFilter)
+            v_gpuimage_view.requestRender()
         }
     }
 
