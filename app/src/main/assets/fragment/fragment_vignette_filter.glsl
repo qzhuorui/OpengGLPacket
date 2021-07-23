4879a2672a4ea8f6af7fd8edb7c9f413eac257cb
ChoRyan Quan <choryanquan@163.com>
2021-07-23 16:07:56 +0800

    fix: 1.vignette fragment shader

--- app/src/main/assets/fragment/fragment_vignette_filter.glsl
+++ app/src/main/assets/fragment/fragment_vignette_filter.glsl
@@ -2,14 +2,20 @@ uniform sampler2D inputImageTexture;
 varying highp vec2 textureCoordinate;
 
 uniform lowp vec2 vignetteCenter;
-uniform lowp vec2 vignetteColor;
+uniform lowp vec3 vignetteColor;
 uniform highp float vignetteStart;
 uniform highp float vignetteEnd;
 
 void main() {
-    lowp vec3 rgb = texture2D(inputImageTexture, textureCoordinate).rgb;
-    lowp float distance = distance(textureCoordinate, vignetteCenter);
-    rgb *= (1.0 - smoothstep(vignetteStart, vignetteEnd, distance));
-    gl_FragColor = vec4(vec3(rgb), 1.0);
+    //    lowp vec3 rgb = texture2D(inputImageTexture, textureCoordinate).rgb;
+    //    lowp float distance = distance(textureCoordinate, vignetteCenter);
+    //    rgb *= (1.0 - smoothstep(vignetteStart, vignetteEnd, distance));
+    //    gl_FragColor = vec4(vec3(rgb), 1.0);
+
+    lowp vec4 color = texture2D(inputImageTexture, textureCoordinate);
+    lowp float d = distance(textureCoordinate, vignetteCenter);
+    lowp float percent = smoothstep(vignetteStart, vignetteEnd, d);
+    gl_FragColor = vec4(mix(color.rgb, vignetteColor, percent), color.a);
+
 
 }
