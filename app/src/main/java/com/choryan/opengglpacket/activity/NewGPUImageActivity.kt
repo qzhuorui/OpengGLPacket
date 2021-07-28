ab388adc210538428624d7f0f7b44304a57ff2b0
ChoRyan Quan <choryanquan@163.com>
2021-07-28 10:19:34 +0800

    fix: 1.GPUImage结构优化-v4

--- app/src/main/java/com/choryan/opengglpacket/activity/NewGPUImageActivity.kt
+++ app/src/main/java/com/choryan/opengglpacket/activity/NewGPUImageActivity.kt
@@ -4,29 +4,42 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Toast
 import com.choryan.opengglpacket.R
 import com.choryan.opengglpacket.base.BaseActivity
 import com.choryan.opengglpacket.filter.GPUImageGrayscaleFilter
 import com.choryan.opengglpacket.gpuImage.GPUImage
-import kotlinx.android.synthetic.main.activity_draw_bitmap_sobel.*
+import com.choryan.opengglpacket.gpuImage.GPUImageFilter
+import com.choryan.opengglpacket.view.SelectFilterPop
+import kotlinx.android.synthetic.main.activity_draw_bitmap_filter.*
+import kotlinx.android.synthetic.main.activity_draw_bitmap_sobel.v_gpuimage_view
 
 /**
  * @author: ChoRyan Quan
  * @date: 2021/6/22
  */
-class NewGPUImageActivity : BaseActivity(R.layout.activity_draw_bitmap_filter) {
+class NewGPUImageActivity : BaseActivity(R.layout.activity_draw_bitmap_filter), SelectFilterPop.SelectFilterCallBack {
 
+    private val selFilterPop by lazy {
+        val pop = SelectFilterPop(this)
+        pop
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val sourceBitmap = BitmapFactory.decodeResource(resources, R.mipmap.teststicker)
         val dstBitmap = BitmapFactory.decodeResource(resources, R.mipmap.test_bitmap)
         v_gpuimage_view.setImageInput(GPUImage(dstBitmap))
-        btn_render.setOnClickListener {
-            v_gpuimage_view.addFilter(GPUImageGrayscaleFilter())
+        btn_select_filter.setOnClickListener {
+            selFilterPop.show(it)
         }
-        btn_remove_render.setOnClickListener {
+        btn_remove_filter.setOnClickListener {
+            Toast.makeText(this, "remove filter", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun provideSelFilter(gpuImageFilter: GPUImageFilter) {
+        v_gpuimage_view.addFilter(gpuImageFilter)
     }
 
     companion object {
